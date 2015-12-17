2c4c28555220f8ca80df2683b9fbb48573197be8
RWOverdijk <r.w.overdijk@gmail.com>
2015-12-17 13:58:02 +0100

    chore(project): Created new build

--- dist/es6/index.js
+++ dist/es6/index.js
@@ -1,6 +1,6 @@
 import {HttpClient} from 'aurelia-fetch-client';
 export {Rest} from './rest';
 
-export function configure (aurelia, configCallback) {
+export function configure(aurelia, configCallback) {
   aurelia.container.get(HttpClient).configure(configCallback);
 }
