6506ac25a749971e1165f69a28617f6865e07667
RWOverdijk <r.w.overdijk@gmail.com>
2015-12-17 13:54:08 +0100

    feat(index): Added configure convenience method

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import {HttpClient} from 'aurelia-fetch-client';
 export {Rest} from './rest';
+
+export function configure (aurelia, configCallback) {
+  aurelia.container.get(HttpClient).configure(configCallback);
+}
