720620347e05536488178bc8238236373ee812fe
RWOverdijk <r.w.overdijk@gmail.com>
2016-01-15 18:00:33 +0100

    refactor(test): Improve test command

--- build/tasks/test.js
+++ build/tasks/test.js
@@ -1,34 +1,45 @@
-var gulp = require('gulp');
-var karma = require('karma').server;
+var gulp        = require('gulp');
+var KarmaServer = require('karma').Server;
+var server      = require('./server');
 
 /**
  * Run test once and exit
  */
-gulp.task('test', function (done) {
-    karma.start({
-        configFile: __dirname + '/../../karma.conf.js',
-        singleRun: true
-    }, function(e) {
+gulp.task('test', ['lint'], function(done) {
+  server.start(function() {
+    var karmaServer = new KarmaServer({
+      configFile: __dirname + '/../../karma.conf.js',
+      singleRun: true
+    }, function() {
+      server.stop(function () {
         done();
+
+        process.exit();
+      });
     });
+
+    karmaServer.start();
+  });
 });
 
 /**
  * Watch for file changes and re-run tests on each change
  */
-gulp.task('tdd', function (done) {
-    karma.start({
-        configFile: __dirname + '/../../karma.conf.js'
-    }, function(e) {
-        done();
-    });
+gulp.task('tdd', function(done) {
+  var karmaServer = new KarmaServer({
+    configFile: __dirname + '/../../karma.conf.js'
+  }, function() {
+    done();
+  });
+
+  karmaServer.start();
 });
 
 /**
  * Run test once with code coverage and exit
  */
-gulp.task('cover', function (done) {
-  karma.start({
+gulp.task('cover', function(done) {
+  var karmaServer = new KarmaServer({
     configFile: __dirname + '/../../karma.conf.js',
     singleRun: true,
     reporters: ['coverage'],
@@ -40,7 +51,9 @@ gulp.task('cover', function (done) {
       type: 'html',
       dir: 'build/reports/coverage'
     }
-  }, function (e) {
+  }, function() {
     done();
   });
+
+  karmaServer.start();
 });
