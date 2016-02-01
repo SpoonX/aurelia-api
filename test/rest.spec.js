bff9fd82d693ccabdfe5fee81f0a90106d9ad01c
doktordirk <doktordirk@outlook.de>
2016-02-01 19:20:03 +0100

    chore(test/rest): more tests to detect accidental api changes

--- test/rest.spec.js
+++ test/rest.spec.js
@@ -13,6 +13,15 @@ let baseUrls  = {
 config.registerEndpoint('api', baseUrls.api);
 config.registerEndpoint('github', baseUrls.github);
 
+let criteria = {user: 'john', comment: 'last'};
+let body = {message: 'some'};
+let options = {
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': 'Bearer aToken'
+  }
+};
+
 describe('Rest', function() {
   describe('.find()', function() {
     it('Should find results for multiple endpoints.', function(done) {
@@ -28,11 +37,77 @@ describe('Rest', function() {
           }),
         injectTest.apiEndpoint.find('posts')
           .then(y => {
+            expect(y.method).toBe('GET');
+          }),
+        injectTest.apiEndpoint.find('posts')
+          .then(y => {
             expect(y.path).toBe('/posts');
+          }),
+        injectTest.apiEndpoint.find('posts', 'id')
+          .then(y => {
+            expect(y.path).toBe('/posts/id');
+            expect(JSON.stringify(y.query)).toBe('{}');
+          }),
+        injectTest.apiEndpoint.find('posts', criteria)
+          .then(y => {
+            expect(y.path).toBe('/posts');
+            expect(JSON.stringify(y.query)).toBe(JSON.stringify(criteria));
+          }),
+        injectTest.apiEndpoint.find('posts', undefined, options)
+          .then(y => {
+            expect(y.path).toBe('/posts');
+            expect(y.contentType).toBe(options.headers['Content-Type']);
+            expect(y.Authorization).toBe(options.headers['Authorization']);
           })
       ]).then(x => {
         done();
       });
+    });
+  });
+
+  describe('.update()', function() {
+    it('Should update with body (as json), criteria and options.', function(done) {
+      let injectTest = container.get(InjectTest);
+
+      injectTest.apiEndpoint.update('posts', criteria, body, options)
+        .then(y => {
+          expect(y.method).toBe('PUT');
+          expect(y.path).toBe('/posts');
+          expect(JSON.stringify(y.query)).toBe(JSON.stringify(criteria));
+          expect(y.contentType).toBe('application/json');
+          expect(y.Authorization).toBe(options.headers['Authorization']);
+          done();
+        });
+    });
+  });
+
+  describe('.destroy()', function() {
+    it('Should destroy with id and options.', function(done) {
+      let injectTest = container.get(InjectTest);
+
+      injectTest.apiEndpoint.destroy('posts', 'id', options)
+        .then(y => {
+          expect(y.method).toBe('DELETE');
+          expect(y.path).toBe('/posts/id');
+          expect(JSON.stringify(y.query)).toBe('{}');
+          expect(y.Authorization).toBe(options.headers['Authorization']);
+          done();
+        });
+    });
+  });
+
+  describe('.create()', function() {
+    it('Should create body (as json) and options.', function(done) {
+      let injectTest = container.get(InjectTest);
+
+      injectTest.apiEndpoint.create('posts', body, options)
+        .then(y => {
+          expect(y.method).toBe('POST');
+          expect(y.path).toBe('/posts');
+          expect(y.contentType).toBe('application/json');
+          expect(y.Authorization).toBe(options.headers['Authorization']);
+          done();
+        });
     });
   });
 });
