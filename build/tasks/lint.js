2456f516b58fbe63da9bf5ddc2a7b531bba61824
doktordirk <doktordirk@outlook.de>
2016-02-14 14:00:29 +0100

    chore(markdown): added markdown linter

--- build/tasks/lint.js
+++ build/tasks/lint.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp');
 var paths = require('../paths');
 var eslint = require('gulp-eslint');
+var remark = require('gulp-remark');
 
 gulp.task('lint', function() {
   return gulp.src(paths.source)
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failOnError());
+});
+
+gulp.task('md-lint', function() {
+  gulp.src(['*.md', paths.doc + '/*.md', '!' + paths.doc + '/CHANGELOG.md'])
+    .pipe(remark());
 });
