45421e0b201971973f4a581323786d5d66a008e2
doktordirk <doktordirk@outlook.de>
2016-03-21 11:27:25 +0100

    chore(project): use babel6, some preparation for jspm0.17,
    * sort package.json
    * remove unused do task
    * add eslintrc to build folder

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -5,53 +5,34 @@ var paths = require('../paths');
 var compilerOptions = require('../babel-options');
 var assign = Object.assign || require('object.assign');
 
-gulp.task('build-html-es6', function () {
-  return gulp.src(paths.html)
-    .pipe(gulp.dest(paths.output + 'es6'));
-});
-
-gulp.task('build-es6', ['build-html-es6'], function () {
+gulp.task('build-es2015', function() {
   return gulp.src(paths.source)
-    .pipe(gulp.dest(paths.output + 'es6'));
+    .pipe(to5(assign({}, compilerOptions.es2015())))
+    .pipe(gulp.dest(paths.output + 'es2015'));
 });
 
-gulp.task('build-html-commonjs', function () {
-  return gulp.src(paths.html)
+gulp.task('build-commonjs', function() {
+  return gulp.src(paths.source)
+    .pipe(to5(assign({}, compilerOptions.commonjs())))
     .pipe(gulp.dest(paths.output + 'commonjs'));
 });
 
-gulp.task('build-commonjs', ['build-html-commonjs'], function () {
+gulp.task('build-amd', function() {
   return gulp.src(paths.source)
-    .pipe(to5(assign({}, compilerOptions, {modules:'common'})))
-    .pipe(gulp.dest(paths.output + 'commonjs'));
-});
-
-gulp.task('build-html-amd', function () {
-  return gulp.src(paths.html)
+    .pipe(to5(assign({}, compilerOptions.amd())))
     .pipe(gulp.dest(paths.output + 'amd'));
 });
 
-gulp.task('build-amd', ['build-html-amd'], function () {
+gulp.task('build-system', function() {
   return gulp.src(paths.source)
-    .pipe(to5(assign({}, compilerOptions, {modules:'amd'})))
-    .pipe(gulp.dest(paths.output + 'amd'));
-});
-
-gulp.task('build-html-system', function () {
-  return gulp.src(paths.html)
-    .pipe(gulp.dest(paths.output + 'system'));
-});
-
-gulp.task('build-system', ['build-html-system'], function () {
-  return gulp.src(paths.source)
-    .pipe(to5(assign({}, compilerOptions, {modules:'system'})))
+    .pipe(to5(assign({}, compilerOptions.system())))
     .pipe(gulp.dest(paths.output + 'system'));
 });
 
 gulp.task('build', function(callback) {
   return runSequence(
     'clean',
-    ['build-es6', 'build-commonjs', 'build-amd', 'build-system'],
+    ['build-es2015', 'build-commonjs', 'build-amd', 'build-system'],
     callback
   );
 });
