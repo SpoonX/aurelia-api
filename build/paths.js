1585c9ae863387254600e2567c2666739faa56ff
doktordirk <doktordirk@outlook.de>
2016-04-10 10:51:20 +0200

    chore(task): fix bundle. needs imports and resources

--- build/paths.js
+++ build/paths.js
@@ -14,6 +14,7 @@ module.exports = {
     appRoot + '**/*.js',          // list files to parse for d.ts
     '!' + appRoot + entryFileName  // exclude entry file
   ],
+  resources: '', // relative to root, resources can not that easily be bundled into a single file (due to naming conventions)
   html: appRoot + '**/*.html',
   style: 'styles/**/*.css',
   output: 'dist/',
