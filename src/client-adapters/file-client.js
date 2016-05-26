ad34d509dd2337a6c3f74069481975b71b63b161
doktordirk <doktordirk@outlook.de>
2016-05-26 13:37:38 +0200

    feat(file-client): add file client adapter

---
+++ src/client-adapters/file-client.js
@@ -0,0 +1,67 @@
+import {parseQueryString, join} from 'aurelia-path';
+import {DefaultLoader} from 'aurelia-loader-default';
+import {findSelected} from './util';
+
+export class FileClient {
+  /**
+  * base path
+  */
+  baseUrl = '';
+
+  /**
+  * builder object
+  */
+  builder = {
+    withBaseUrl: baseUrl => {
+      this.baseUrl = baseUrl;
+      return baseUrl;
+    },
+    withLoader: loader => this.loader = loader
+  };
+
+  /**
+  * Creates an instance of FileClient.
+   *
+   * @param {loader} File loader with loader.loadText(path). default: (aurelia-)DefaultLoader
+   */
+  constructor(loader = new DefaultLoader()) {
+    this.loader = loader;
+  }
+
+  /**
+  * Configure this client with default settings to be used by all requests.
+  *
+  * @param config A configuration object, or a function that takes a config
+  * object and configures it.
+  * @returns The chainable instance of this FileClient.
+  * @chainable
+  */
+   configure(_configure) {
+    if (typeof _configure === 'function') {
+      _configure(this.builder);
+    } else if (typeof _configure === 'string') {
+      this.baseUrl = _configure;
+    }
+
+    return this;
+  }
+
+  /**
+  * Fetches a resource. Default configuration parameters
+  * will be applied.
+  *
+  * @param path A string containing the URL of the resource.
+  * @returns A Promise<Object|Error> with the response
+  */
+  loadJson(path) {
+    let [, pathKey, , id, , query] = /^([^\/^\?]+)(\/)?([^\/^\?]+)?(\?)?(.+)?/.exec(path);
+
+    let queryParameters = parseQueryString(query);
+    if (id) queryParameters.id = id;
+
+    return this.loader.loadText(join(this.baseUrl, pathKey) + '.json').then(text => {
+      let resource = JSON.parse(text);
+      return findSelected(resource, queryParameters) || [];
+    });
+  }
+}
