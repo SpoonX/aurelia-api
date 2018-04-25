06cbca6f8e76ab0cc123363ea5f6d180fe99af2c
doktordirk <doktordirk@outlook.de>
2018-04-25 15:17:00 +0200

    chore(test): added test for applying registered default headers

--- test/resources/inject-test.js
+++ test/resources/inject-test.js
@@ -1,11 +1,12 @@
 import {inject} from 'aurelia-dependency-injection';
 import {Endpoint} from '../../src/endpoint';
 
-@inject(Endpoint.of('api'), Endpoint.of('jsonplaceholder'), Endpoint.of('form'))
+@inject(Endpoint.of('api'), Endpoint.of('jsonplaceholder'), Endpoint.of('form'), Endpoint.of('urlencoded'))
 export class InjectTest {
-  constructor(apiEndpoint, jsonplaceholderEndpoint, formEndpoint) {
+  constructor(apiEndpoint, jsonplaceholderEndpoint, formEndpoint, urlencodedEndpoint) {
     this.apiEndpoint    = apiEndpoint;
     this.jsonplaceholderEndpoint = jsonplaceholderEndpoint;
     this.formEndpoint = formEndpoint;
-  }
+    this.urlencodedEndpoint = urlencodedEndpoint;
+   }
 }
